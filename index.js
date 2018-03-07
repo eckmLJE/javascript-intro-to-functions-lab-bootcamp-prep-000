9f53dc1b1ea0c04dd0543e97200920e60e040c2b
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-07 15:28:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase)
+  console.log("I can't hear you!")
+}
